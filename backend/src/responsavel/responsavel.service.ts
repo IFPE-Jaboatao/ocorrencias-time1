b2ab39919e37da210ddc062ab38b91208181e651
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-04-21 22:53:14 -0300

    refactor(responsavel): remove upsert and add create/update

--- backend/src/responsavel/responsavel.service.ts
+++ backend/src/responsavel/responsavel.service.ts
@@ -2,10 +2,9 @@ import {
   BadRequestException,
   ConflictException,
   Injectable,
-  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, QueryFailedError, Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { Responsavel } from './responsavel.entity';
 import { Aluno } from 'src/aluno/aluno.entity';
 
@@ -33,58 +32,66 @@ export class ResponsavelService {
     });
   }
 
-  async upsertResponsavel(
-    dados: Partial<Responsavel>,
+  async update(
+    id: number,
+    data: Partial<Responsavel>,
     alunoIds: number[],
-  ): Promise<Responsavel> {
-    if (!dados.cpf) {
-      throw new BadRequestException('CPF é obrigatório');
-    }
-
+  ): Promise<Responsavel | null> {
     try {
-      let responsavel = await this.findByCpf(dados.cpf);
+      let alunos = [];
 
-      if (responsavel) {
-        // Evita update vazio
-        if (Object.keys(dados).length > 0) {
-          Object.assign(responsavel, dados);
-        }
-      } else {
-        responsavel = this.responsavelRepository.create(dados);
-      }
-
-      const responsavelSalvo =
-        await this.responsavelRepository.save(responsavel);
-
-      // 🔗 vincular alunos
       if (alunoIds && alunoIds.length > 0) {
-        const alunos = await this.alunoRepository.findBy({
+        alunos = await this.alunoRepository.findBy({
           id: In(alunoIds),
         });
-
-        if (alunos.length !== alunoIds.length) {
-          throw new NotFoundException(
-            'Um ou mais alunos não foram encontrados',
-          );
-        }
-
-        alunos.forEach((aluno) => {
-          aluno.responsavel = responsavelSalvo;
-        });
-
-        await this.alunoRepository.save(alunos);
       }
 
-      return responsavelSalvo;
+      await this.responsavelRepository.update(id, {
+        nome: data.nome,
+        email: data.email,
+        telefone: data.telefone,
+        cpf: data.cpf,
+        usuario: data.usuario,
+        alunos: alunos,
+      });
+
+      return this.responsavelRepository.findOneBy({ id });
     } catch (error) {
-      // erro de duplicidade (MySQL)
-      if (error instanceof QueryFailedError) {
-        if ((error as any).code === 'ER_DUP_ENTRY') {
-          throw new ConflictException('Responsável já existe');
-        }
+      console.error(error);
+      throw new BadRequestException('Erro ao atualizar responsável');
+    }
+  }
+
+  async create(
+    data: Partial<Responsavel>,
+    alunoIds: number[],
+  ): Promise<Responsavel> {
+    try {
+      let alunos = [];
+
+      if (alunoIds && alunoIds.length > 0) {
+        alunos = await this.alunoRepository.findBy({
+          id: In(alunoIds),
+        });
       }
 
-      throw new BadRequestException('Erro ao salvar responsável');
+      const newResponsavel = this.responsavelRepository.create({
+        nome: data.nome,
+        email: data.email,
+        telefone: data.telefone,
+        cpf: data.cpf,
+        usuario: data.usuario,
+        alunos: alunos,
+      });
+
+      const save = await this.responsavelRepository.save(newResponsavel);
+      return save;
+    } catch (error) {
+      if ((error as any).code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('Responsável já existe');
+      }
+
+      throw new BadRequestException('Erro ao criar responsável');
     }
   }
 }
