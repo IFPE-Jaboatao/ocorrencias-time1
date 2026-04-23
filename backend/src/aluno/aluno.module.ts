adc5d1de23450cfff4ee8861c487a462986f7ac1
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-04-23 15:40:17 -0300

    fix(aluno): resolver dependência circular com OcorrenciaModule e AuthModule usando forwardRef

--- backend/src/aluno/aluno.module.ts
+++ backend/src/aluno/aluno.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AlunoService } from './aluno.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Aluno } from './aluno.entity';
 
 import { AlunoController } from './aluno.controller';
 import { OcorrenciaModule } from '../ocorrencia/ocorrencia.module';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
-  imports: [TypeOrmModule.forFeature([Aluno]), OcorrenciaModule],
+  imports: [
+    TypeOrmModule.forFeature([Aluno]),
+    forwardRef(() => OcorrenciaModule), // Evita dependência circular
+    forwardRef(() => forwardRef(() => AuthModule)), // Evita dependência circular
+  ],
   controllers: [AlunoController],
   providers: [AlunoService],
   exports: [AlunoService],
