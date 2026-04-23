355363973944efe6af366faed85cd9e91e50940c
jaldsonarthur17 <jaldsonarthur1322@gmail.com>
2026-04-22 21:07:52 -0300

    atualizando o aluno.modules

--- backend/src/aluno/aluno.module.ts
+++ backend/src/aluno/aluno.module.ts
@@ -3,9 +3,13 @@ import { AlunoService } from './aluno.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Aluno } from './aluno.entity';
 
+import { AlunoController } from './aluno.controller';
+import { OcorrenciaModule } from '../ocorrencia/ocorrencia.module';
 @Module({
   imports: [TypeOrmModule.forFeature([Aluno])],
+  controllers: [AlunoController],
   providers: [AlunoService],
   exports: [AlunoService],
+  modules: [OcorrenciaModule],
 })
 export class AlunoModule {}
