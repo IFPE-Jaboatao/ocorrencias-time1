a4694f183ecd4d30749cbd3f21642ca140c9adb7
karolynne-freire <freirekarolynne@gmail.com>
2026-05-02 16:00:00 -0300

    chore: corrigindo as credenciais do banco no app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,5 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+import { ConfigModule } from '@nestjs/config'; 
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,13 +8,16 @@ import { ProfessorModule } from './professor/professor.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      host: process.env.DB_HOST, 
+      port: Number(process.env.DB_PORT) || 3306,
+      username: process.env.MYSQL_USER, 
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
       charset: 'utf8mb4',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
