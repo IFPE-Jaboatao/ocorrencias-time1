8aca7ec757f39d6a0ae6b4c9e5b69b696fe2f73d
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-05-03 14:08:12 -0300

    feat: configure Swagger documentation metadata

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,11 +4,23 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-//adicionada importação do ValidationPipe para ativar a validação dos DTOs
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //adicionado o global pipe para 'ativação do DTO' e validação de dados Que vão entrar'
+
+  const config = new DocumentBuilder()
+    .setTitle('IFlow API')
+    .setDescription(
+      'API responsável pela gestão de ocorrências acadêmicas e disciplinares, permitindo registro, acompanhamento, notificações e auditoria com segurança e transparência.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
