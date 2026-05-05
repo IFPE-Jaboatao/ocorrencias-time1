6488c844b52abeb913ff35166397a35b83358681
karolynne-freire <freirekarolynne@gmail.com>
2026-05-05 18:25:44 -0300

    feat: finalizando fluxo de autenticação com botão de logout e persistência na tela admin

--- frontend/src/app/dashboard/admin/page.tsx
+++ frontend/src/app/dashboard/admin/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 import { useAuth } from "@/contexts/AuthContext";
-import { Spinner, Card } from "flowbite-react";
+import { Spinner, Card, Button } from "flowbite-react";
+import { HiOutlineLogout } from "react-icons/hi";
 
 export default function AdminDashboard() {
-  const { user, loading } = useAuth();
+  const { user, loading, logout } = useAuth();
 
   if (loading) {
     return (
@@ -18,9 +19,22 @@ export default function AdminDashboard() {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">
-        Painel Administrativo
-      </h1>
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold text-gray-800">
+          Painel Administrativo
+        </h1>
+
+        <Button
+          color="failure"
+          size="sm"
+          onClick={logout}
+          className="flex items-center gap-2"
+        >
+          <HiOutlineLogout className="h-5 w-5 mr-2" />
+          Sair do iFlow
+        </Button>
+      </div>
+
       <Card>
         <h5 className="text-xl font-bold tracking-tight text-gray-900">
           Bem-vinda, {user?.name}!
